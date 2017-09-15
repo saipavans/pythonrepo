7405757bd6b4cf074d33110b95f4b63abc930c32
= <sp403250@dal.ca>
2017-09-15 14:30:31 -0300

    enhanced for more closer results

--- lab3/task1.py
+++ lab3/task1.py
@@ -2,10 +2,11 @@ x = 10
 a = 4
 def square_root(a):
 	global x
+	epsilon = 0.0000001
 	while True:
 		print (x)
 		y = ((x+(a/x))/2)
-		if y == x:
+		if abs(y-x) < epsilon:
 			break
 		x = y
 	return (x)
