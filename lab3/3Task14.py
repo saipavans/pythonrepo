c363ba9d98e26734953c669629c7654b63c00a76
saipavans <sp403250@dal.ca>
2017-09-18 09:43:00 -0300

    task 14 of lab 3 added

---
+++ lab3/3Task14.py
@@ -0,0 +1,13 @@
+def double_let(word):
+	double_occurence_counter = 0
+	while i < len(word)-1:
+		if word[i] == word[i+1]
+			double_occurence_counter+=1
+			i+=2 # incrementing by 2, to avoid mistaking three 'aaa' as two time 'aa'
+		else
+			i+=1
+			double_occurene_counter=0 ## break of pattern
+		return (double_occurence_counter == 3)
+
+
+					
