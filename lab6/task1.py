8c5a2e1eac0aae283d78c5ea8a24247556fab4de
saipavans <sp403250@dal.ca>
2017-09-27 10:33:10 -0300

    added task 1 for lab 7

---
+++ lab6/task1.py
@@ -0,0 +1,33 @@
+import math
+
+
+class Point:
+
+    x = 0
+    y = 0
+
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def distance_between_points(self, other_point):
+        x_distance = abs(self.x - other_point.x)
+        y_distance = abs(self.y - other_point.y)
+        distance = math.pow(((x_distance**2 + y_distance**2)),0.5)
+        return distance
+
+    def __add__(self, other_point):
+        new_x = self.x + other_point.x
+        new_y = self.y + other_point.y
+        return Point(new_x,new_y)
+
+
+def main():
+    origin = Point(0,0)
+    point1 = Point(1,1)
+    print("Distance between origin and (1,1):", origin.distance_between_points(point1))
+    new_point = origin + point1
+    print("New point:", new_point.x, new_point.y)
+
+if __name__ == '__main__':
+    main()
