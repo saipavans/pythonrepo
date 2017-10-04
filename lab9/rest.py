c22c9d5317568c7fd5c3598601e64426b0738686
saipavans <sp403250@dal.ca>
2017-10-04 10:37:27 -0300

    fetched network host details and displayed on console

--- lab9/rest.py
+++ lab9/rest.py
@@ -3,8 +3,19 @@ import json
 
 base_url = "https://devnetapi.cisco.com/sandbox/apic_em"
 api_url = "/api/v1/ticket"
+service_ticket = ""
+request_header = {'Content-Type':'application/json'}
+credentials_dict =  {'username': 'devnetuser', 'password':'Cisco123!'}
+request_data = credentials_dict
 
+## api call to set the service ticket param ##
+response = requests.post(base_url + api_url, headers={'Content-Type':'application/json'}, data= json.dumps(request_data))
+response_json = response.json()
+service_ticket = response_json['response']['serviceTicket']
 
-response = requests.post(base_url + api_url, headers={'Content-Type':'application/json'}, data= json.dumps({'username': 'devnetuser', 'password':'Cisco123!'}))
-
-print(response.json())
+## api call to get network hosts details ##
+api_url = "/api/v1/host"
+request_header['X-Auth-Token'] = service_ticket  ## setting the service ticket fetched previously to the request header
+response = requests.get(base_url + api_url, headers= request_header)
+response_json = response.json()
+print(response_json)
